b7254f6e4464dc2e97d99d0f31a1923303c77c2e
surini <surini@gmail.com>
2019-06-06 16:41:06 +0300

    10

--- maqueen.ts
+++ maqueen.ts
@@ -137,7 +137,7 @@ namespace maqueen{
     
     //% weight=90
     //% blockId=motor_MotorRun block="Motor|%index|dir|%Dir|speed|%speed"
-    //% speed.min=-255 speed.max=255
+    //% speed.min=0 speed.max=255
     //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2
     //% direction.fieldEditor="gridpicker" direction.fieldOptions.columns=2
     export function MotorRun(index: aMotors, direction:Dir, speed: number): void {
